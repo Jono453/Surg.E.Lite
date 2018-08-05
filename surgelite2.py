a76da1e9c7cfd5aaae978a610261d1615383e8d5
Jono453 <32658975+Jono453@users.noreply.github.com>
2018-08-05 10:39:57 +1000

    Final working Python and OpenCV

--- surgelite2.py
+++ surgelite2.py
@@ -29,7 +29,7 @@ ImageHeight = 240
 
 camera = PiCamera()
 camera.resolution = (ImageWidth, ImageHeight)
-camera.framerate = 20
+camera.framerate = 15
 frameCapture = PiRGBArray(camera,size=(ImageWidth,ImageHeight))
 
 def writeNumber(value):
@@ -45,7 +45,7 @@ for frame in camera.capture_continuous(frameCapture, format = "bgr", use_video_p
     image = frame.array
 
     #start image processing to track object (purple mesh hat)    
-    image = cv2.flip(image,0)
+    #image = cv2.flip(image,0)
     image = cv2.flip(image,1)
 
     image = cv2.medianBlur(image,5)
@@ -59,7 +59,7 @@ for frame in camera.capture_continuous(frameCapture, format = "bgr", use_video_p
     #Threholding for red color
     mask = cv2.inRange(image,lower_red, high_red)
 
-    result = cv2.bitwise_and(image,image,mask=mask)
+    result = cv2.bitwise_and(image,image,mask=mask) 
 
     #Bounding box and Error
     img, contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
@@ -67,7 +67,7 @@ for frame in camera.capture_continuous(frameCapture, format = "bgr", use_video_p
     #Constants
     frame_x = ImageWidth/2
     frame_y = ImageHeight/2
-    min_area = 230
+    min_area = 275
     
     for c in contours: #loop over contours array
     
@@ -107,28 +107,30 @@ for frame in camera.capture_continuous(frameCapture, format = "bgr", use_video_p
         else:
             is_top = 0
             #print("TOP")
-        
+
+        var = 0
+        #Quadrant Calculations:
         if is_left == 1 and is_top == 1:
-            #print("Sending -- 1")
+            print("Front Right")
             var = 1
         elif is_left == 0 and is_top == 1:
-            #print("Sending -- 2")
+            print("Back Right")
             var = 2
         elif is_left == 1 and is_top == 0:
-            #print("Sending -- 4")
-            var =  4
+            print("Front Left")
+            var = 4
         else:
-            #print("Sending -- 3")
+            print("Back Left")
             var = 3
-     
-        '''
+        
+        #sending variables to Arduino by i2c for LED control
         writeNumber(var) #var = 1,2,3,4 (regions of light)
-        print("ROI detected: ",var)
+        #print("ROI: ",var)
         time.sleep(0.5)
 
         number = readNumber()
-        print("I received: ", number)
-        '''    
+        #print("I received: ", number)
+        
     
     #To end streaming
     key = cv2.waitKey(5) & 0xFF
@@ -136,5 +138,7 @@ for frame in camera.capture_continuous(frameCapture, format = "bgr", use_video_p
     if key == ord("q"):
         break
 
+
+   
 
     
